a6194458d7397a182072cffb188e8e3cf8e1822f
Onge <onge.uk@gmail.com>
2017-11-06 14:53:37 +0000

    Animate scoreboard change

--- es6/src/scripts/scoring/scoreboard.js
+++ es6/src/scripts/scoring/scoreboard.js
@@ -10,7 +10,26 @@ export default function updateScoreBoard() {
 	// Cache selectors
 	const [score, lives] = [document.getElementById("score"), document.getElementById("lives")];
 
+	// Animate scoreboard data change
+	scoreboardPulsate(score, score.textContent, gs.score || 0);
+	scoreboardPulsate(lives, lives.textContent, gs.lives || 3);
+
 	// Update DOM
 	score.textContent = gs.score;
 	lives.textContent = gs.lives;
+}
+
+// Handle scoreboard pulsation animation
+function scoreboardPulsate(el, prev, next) {
+	if (prev !== next.toString()) {
+		const className = "scoreboard__item_changed";
+
+		// Add animation class
+		el.classList.add(className);
+
+		el.addEventListener("animationend", () => {
+			// Once animation is over, remove animation class
+			el.classList.remove(className);
+		});
+	}
 }
